e41b32160ea037c2146de5cab821250d17c4fb97
Burak Haşıcı <burakhasici@gmail.com>
2022-04-20 15:08:46 +0300

    first commit

---
+++ src/components/Form.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Form() {
+    return (
+        <form action="">
+            <input type="text" className="new-todo" placeholder="What needs to be done?" autoFocus={null}/>
+        </form>
+    );
+}
+
+export default Form;
